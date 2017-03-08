6df4ffa0a54ea81ab9caca7344bf70ec8fdaec0f
colemainia <choyt@mymail.mines.edu>
2017-03-08 15:21:57 -0700

    Maybe ready to build some jUnit Test Cases now. ChessBoard.java is more of a complete skeleton

--- src/ChessBoard/ChessBoard.java
+++ src/ChessBoard/ChessBoard.java
@@ -1,5 +1,25 @@
 package ChessBoard;
 
 public class ChessBoard {
-	private BoardSquare[][] board;
+	public static final int BOARD_SIZE = 8;
+	private static ChessBoard theBoard = new ChessBoard();
+	private BoardSquare[][] chessBoard;
+	public static ChessBoard getBoard() {
+		return theBoard;
+	}
+	public void initialize() {
+		chessBoard = new BoardSquare[BOARD_SIZE][BOARD_SIZE];
+		for (int i = 0; i < BOARD_SIZE; i++) {
+			for (int j = 0; j < BOARD_SIZE; j++) {
+				chessBoard[i][j] = new BoardSquare(i, j, ChessPiece.None, OccupiedBy.None);
+			}
+		}
+	}
+	public void SetBoard() {
+		
+	}
+	public void ChangeSquare(int rank, int file, ChessPiece chessPiece, OccupiedBy occupiedBy) {
+		
+	}
+	public BoardSquare getSquare(int rank, int file) {return chessBoard[rank][file];}
 }
