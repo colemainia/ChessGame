40761ef948630a50fefb0c2897a51b199e563a3e
colemainia <choyt@mymail.mines.edu>
2017-03-08 19:24:07 -0700

    Board has getters and an initialize. Simpleton constructor, we only need one board at a time

--- src/ChessBoard/ChessBoard.java
+++ src/ChessBoard/ChessBoard.java
@@ -1,25 +1,30 @@
 package ChessBoard;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ChessBoard {
 	public static final int BOARD_SIZE = 8;
 	private static ChessBoard theBoard = new ChessBoard();
 	private BoardSquare[][] chessBoard;
-	public static ChessBoard getBoard() {
-		return theBoard;
-	}
+	private Set<BoardSquare> whitePieces;
+	private Set<BoardSquare> blackPieces;
+	public static ChessBoard getBoard() {return theBoard;}
 	public void initialize() {
 		chessBoard = new BoardSquare[BOARD_SIZE][BOARD_SIZE];
+		whitePieces = new HashSet<BoardSquare>();
+		blackPieces = new HashSet<BoardSquare>();
+	}
+	public void SetBoard(String FileName) {
 		for (int i = 0; i < BOARD_SIZE; i++) {
 			for (int j = 0; j < BOARD_SIZE; j++) {
 				chessBoard[i][j] = new BoardSquare(i, j, ChessPiece.None, OccupiedBy.None);
 			}
 		}
 	}
-	public void SetBoard() {
-		
-	}
+	//May be useful? Don't know, but probably not going to be called anytime soon.
 	public void ChangeSquare(int rank, int file, ChessPiece chessPiece, OccupiedBy occupiedBy) {
-		
+		chessBoard[rank][file] = new BoardSquare(rank, file, chessPiece, occupiedBy);
 	}
-	public BoardSquare getSquare(int rank, int file) {return chessBoard[rank][file];}
+	public BoardSquare getSquareAt(int rank, int file) {return chessBoard[rank][file];}
 }
